384f56e720ae732b49613f5288ad1a9f3a489fe8
AndreaCatapano <a.catapano96@gmail.com>
2025-03-06 14:57:11 +0100

    feat: added random number

--- js/script.js
+++ js/script.js
@@ -4,8 +4,40 @@ const countdownElement = document.getElementById("countdown");
 const numbersListElement = document.getElementById("numbers-list");
 const answersFormElement = document.getElementById("answers-form");
 
+const numbersListArr = [0, 0, 0, 0, 0];
+
+numbersListElement.textContent
+// Random 
+const randomNumber = () =>  parseInt(Math.random() * (50 - 1) + 1);
+const numbersListGenerated = generateRandomNumber(numbersListArr);
+generateListElement(numbersListGenerated);
+
+
+//Timer
 
 setInterval(function(){
     numbersListElement.classList.add("d-none")
     answersFormElement.classList.remove("d-none")
 }, 10000)
+
+
+
+
+// Funzioni
+function generateRandomNumber(arr) {
+
+    for (let i = 0; i < arr.length; i++){
+        arr[i] = randomNumber();
+    }
+
+   return arr;
+}
+
+function generateListElement (arr) {
+    for(let i = 0; i < arr.length; i++){
+        const newLi = document.createElement("li");
+        newLi.textContent = arr[i];
+        numbersListElement.appendChild(newLi);
+    }
+}
+
